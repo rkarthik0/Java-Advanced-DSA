cd1428dfa61f6f9da4dd222e2b1f1bc5ab518c95
Karthik R <karthik200117@gmail.com>
2026-08-18 21:52:58 +0530

    updated the code

--- src/Algorithms/recursion/PrintNumbers.java
+++ src/Algorithms/recursion/PrintNumbers.java
@@ -1,4 +1,20 @@
-package Algorithms.recursion;
+/**
+ * ============================================================
+ * Problem            : Print Numbers From 1 to A
+ *
+ * Approach           : Recursion
+ *
+ * Time Complexity    : O(A)
+ * Space Complexity   : O(A) due to recursion stack
+ *
+ * Example:
+ *
+ * A = 5
+ *
+ * Output:
+ * 1 2 3 4 5
+ * ============================================================
+ */
 
 public class PrintNumbers {
 }
